cf0929733023ee2f833af8cf95278b54a64ed440
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-03-30 07:07:30 -0300

    Tokens all the way
    scan_groups was complex and slow, the new tokenization system is much
    simpler to maintain. Almost all the JSHOP parser was rewritten, with
    several exceptions added. Eventually the token system will be moved to a
    separate file to be shared between S-Expression parsers.

--- parsers/JSHOP_Parser.rb
+++ parsers/JSHOP_Parser.rb
@@ -1,54 +1,30 @@
 module JSHOP_Parser
   extend self
 
-  OPERATOR_NAME = 0
-  OPERATOR_PREC = 1
-  OPERATOR_DEL_EFF = 2
-  OPERATOR_ADD_EFF = 3
-
-  METHOD_NAME = 0
-  METHOD_PREC  = 1
-  METHOD_SUBTAKS = 2
-
   attr_reader :domain_name, :problem_name, :problem_domain, :operators, :methods, :predicates, :state, :tasks, :goal
 
   #-----------------------------------------------
-  # Scan groups
+  # Scan tokens
   #-----------------------------------------------
 
-  def scan_groups(str)
-    group = ''
-    count_paren = 0
-    str.each_char {|c|
-      case c
-      when '('
-        if count_paren.zero? and not group.empty?
-          yield group
-          group = ''
-        end
-        count_paren += 1
-        group << c
-      when ')'
-        count_paren -= 1
-        group << c
-        if count_paren.zero?
-          yield group
-          group = ''
-        elsif count_paren < 0
-          raise "Unmatched parentheses for #{str}"
-        end
-      else
-        if count_paren.zero?
-          if c =~ /\w/
-            group << c
-          elsif not group.empty?
-            yield group
-            group = ''
-          end
-        else group << c
-        end
-      end
-    }
+  def scan_tokens(str)
+    tokenize(str.scan(/[()]|[!?:]*\w+/))
+  end
+
+  #-----------------------------------------------
+  # Tokenize
+  #-----------------------------------------------
+
+  def tokenize(tokens)
+    raise 'No more tokens found' if tokens.empty?
+    t = tokens.shift
+    if t == '('
+      list = []
+      list << tokenize(tokens) until tokens.first == ')'
+      tokens.shift
+      list
+    else t
+    end
   end
 
   #-----------------------------------------------
@@ -56,63 +32,50 @@ module JSHOP_Parser
   #-----------------------------------------------
 
   def parse_operator(op)
-    operator = Array.new(6)
-    counter = OPERATOR_NAME
-    scan_groups(op) {|value|
-      case counter
-      when OPERATOR_NAME
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.gsub!(/[?!]/,'')
-          i = i.split
-          operator[0] = i.shift
-          operator[1] = i
-        }
-      when OPERATOR_PREC
-        operator[2] = pos = []
-        operator[3] = neg = []
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.gsub!('?','')
-          i.scan(/\(\s*(.+?)\s*\)/) {|j|
-            if j.first =~ /^not\s*\(\s*(.+)\s*$/
-              proposition = $1.split
-              neg << proposition
-            else
-              proposition = j.first.split
-              pos << proposition
-            end
-            @predicates[proposition.first] = true if @predicates[proposition.first].nil?
-          }
-        }
-      when OPERATOR_DEL_EFF
-        operator[5] = del = []
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.gsub!('?','')
-          i.scan(/\(\s*(.+?)\s*\)/) {|j|
-            proposition = j.first.split
-            del << proposition
-            @predicates[proposition.first] = false
-          }
-        }
-      when OPERATOR_ADD_EFF
-        operator[4] = add = []
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.gsub!('?','')
-          i.scan(/\(\s*(.+?)\s*\)/) {|j|
-            proposition = j.first.split
-            add << proposition
-            @predicates[proposition.first] = false
-          }
-        }
-        @operators << operator
-      else raise "Unknow operator group"
-      end
-      counter += 1
-    }
-    raise 'Missing operator groups' if counter != OPERATOR_ADD_EFF.succ
+    op.shift
+    raise "Operator #{op.first.first} have #{op.size} groups instead of 4" if op.size != 4
+    # Header
+    group = op.shift
+    group.each {|i| i.sub!(/^[?!]+/,'')}
+    @operators << [name = group.shift, group, pos = [], neg = [], add = [], del = []]
+    # Preconditions
+    group = op.shift
+    if group != 'nil'
+      raise "Error with #{name} preconditions" unless group.instance_of?(Array)
+      group.each {|pro|
+        if pro.first == 'not'
+          raise "Error with #{name} negative precondition group" if pro.size != 2
+          proposition = pro.last
+          neg << proposition
+        else
+          proposition = pro
+          pos << proposition
+        end
+        proposition.each {|i| i.sub!('?','')}
+        @predicates[proposition.first] = true if @predicates[proposition.first].nil?
+      }
+    end
+    # Effects
+    group = op.shift
+    if group != 'nil'
+      raise "Error with #{name} del effects" unless group.instance_of?(Array)
+      group.each {|proposition|
+        raise "Error with #{name} del effects" if proposition.first == 'not'
+        del << proposition
+        proposition.each {|i| i.sub!('?','')}
+        @predicates[proposition.first] = false
+      }
+    end
+    group = op.shift
+    if group != 'nil'
+      raise "Error with #{name} add effects" unless group.instance_of?(Array)
+      group.each {|proposition|
+        raise "Error with #{name} add effects" if proposition.first == 'not'
+        add << proposition
+        proposition.each {|i| i.sub!('?','')}
+        @predicates[proposition.first] = false
+      }
+    end
   end
 
   #-----------------------------------------------
@@ -120,73 +83,50 @@ module JSHOP_Parser
   #-----------------------------------------------
 
   def parse_method(met)
-    method = []
-    counter = METHOD_NAME
-    label = true
-    complete = false
-    decompose = nil
-    scan_groups(met) {|value|
-      case counter
-      when METHOD_NAME
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.gsub!(/[?!]/,'')
-          i = i.split
-          method << i.shift << i
-        }
-        # Already defined
-        met = @methods.find {|m| m.first == method.first}
-        if met
-          method = met
-        else
-          @methods << method
-        end
-        counter = METHOD_PREC
-      when METHOD_PREC
-        complete = false
-        # Optional label may appear
-        if label
-          label = false
-          if value =~ /\w+/
-            decompose = [value]
-            next
+    met.shift
+    # Header
+    group = met.first
+    group.each {|i| i.sub!(/^[?!]+/,'')}
+    name = group.shift
+    # Already defined
+    method = @methods.find {|m| m.first == name}
+    @methods << method = [name, group] unless method
+    met.shift
+    until met.empty?
+      # Optional label
+      if met.first.instance_of?(String)
+        decompose = [met.shift, free_variables = [], pos = [], neg = []]
+      # Add numbers as labels for the unlabeled cases
+      else decompose = ["unlabeled_#{method.size - 2}"]
+      end
+      # Preconditions
+      group = met.shift
+      if group != 'nil'
+        raise "Error with #{name} preconditions" unless group.instance_of?(Array)
+        group.each {|pro|
+          if pro.first == 'not'
+            raise "Error with #{name} negative precondition group" if pro.size != 2
+            proposition = pro.last
+            neg << proposition
           else
-            # Add numbers as labels for the unlabeled cases
-            decompose = ["unlabeled_#{method.size - 2}"]
+            proposition = pro
+            pos << proposition
           end
-        end
-        decompose << (free_variables = []) << (pos = []) << (neg = [])
-        value.scan(/^\(\s*(.+?)\s*\)$/) {|i|
-          i = i.first
-          i.scan(/\(\s*(.+?)\s*\)/) {|j|
-            if j.first =~ /^not\s*\(\s*(.+)\s*$/
-              proposition = $1.split
-              neg << proposition
-            else
-              proposition = j.first.split
-              pos << proposition
-            end
-            @predicates[proposition.first] = true if @predicates[proposition.first].nil?
-            free_variables.push(*proposition.find_all {|i| i.sub!(/^\?/,'') and not method[1].include?(i)})
-          }
+          free_variables.push(*proposition.find_all {|i| i.sub!(/^\?/,'') and not method[1].include?(i)})
+          @predicates[proposition.first] = true if @predicates[proposition.first].nil?
         }
         free_variables.uniq!
-        counter = METHOD_SUBTAKS
-      when METHOD_SUBTAKS
-        complete = true
-        if value != 'nil'
-          subtasks = []
-          value.gsub!(/[?!]/,'')
-          value.scan(/^\(\s*(.+)\s*\)$/)
-          $1.scan(/\(\s*(.+?)\s*\)/) {|tasks| subtasks << tasks.first.split}
-          decompose << subtasks
-        else decompose << []
-        end
-        method << decompose
-        label = true
-        counter = METHOD_PREC
       end
-    }
+      # Subtasks
+      group = met.shift
+      if group != 'nil'
+        raise "Error with #{name} subtasks" unless group.instance_of?(Array)
+        group.each {|pro| pro.each {|i| i.sub!(/^[?!]+/,'')}}
+        decompose << group
+      else decompose << []
+      end
+      method << decompose
+    end
   end
 
   #-----------------------------------------------
@@ -196,22 +136,23 @@ module JSHOP_Parser
   def parse_domain(domain_filename)
     description = IO.read(domain_filename)
     description.gsub!(/;.*$|\n/,'')
-    if description =~ /^\s*\(\s*defdomain\s+(\w+)\s*\(\s*(.*)\s*\)\s*\)\s*$/
+    tokens = scan_tokens(description)
+    if tokens.instance_of?(Array) and tokens.shift == 'defdomain'
       @operators = []
       @methods = []
-      @domain_name = $1
+      raise 'Found group instead of domain name' if tokens.first.instance_of?(Array)
+      @domain_name = tokens.shift
       @predicates = {}
-      scan_groups($2) {|group|
-        if group =~ /^\s*\(\s*:(operator|method)\s*(.*)\s*\)\s*$/
-          case $1
-          when 'operator'
-            parse_operator($2)
-          when 'method'
-            parse_method($2)
-          end
-        else puts "#{group} is not recognized"
+      raise 'More than one group to define domain content' if tokens.size != 1
+      tokens = tokens.shift
+      until tokens.empty?
+        group = tokens.shift
+        case group.first
+        when ':operator' then parse_operator(group)
+        when ':method' then parse_method(group)
+        else puts "#{group.first} is not recognized"
         end
-      }
+      end
     else raise "File #{domain_filename} does not match domain pattern"
     end
   end
@@ -223,22 +164,13 @@ module JSHOP_Parser
   def parse_problem(problem_filename)
     description = IO.read(problem_filename)
     description.gsub!(/;.*$|\n/,'')
-    if description =~ /^\s*\(\s*defproblem\s+(\w+)\s+(\w+)\s+\(\s*(.+)\s*\)\s*\)\s*$/
-      @problem_name = $1
-      @problem_domain = $2
-      if $3 =~ /(.*)\s*\)\s*\(\s*(.*)/
-        state = $1
-        tasks = $2
-        @state = []
-        @tasks = []
-        state.scan(/\(\s*(.+?)\s*\)/) {|values|
-          proposition = values.first.split
-          @predicates[proposition.first] = nil unless @predicates.include?(proposition.first)
-          @state << proposition
-        }
-        tasks.scan(/\(\s*(.+?)\s*\)/) {|values| @tasks << values.first.split}
-      else raise 'Problem does not define two groups'
-      end
+    tokens = scan_tokens(description)
+    if tokens.instance_of?(Array) and tokens.size == 5 and tokens.shift == 'defproblem'
+      @problem_name = tokens.shift
+      @problem_domain = tokens.shift
+      @state = tokens.shift
+      @state.each {|proposition| @predicates[proposition.first] = nil unless @predicates.include?(proposition.first)}
+      @tasks = tokens.shift
     else raise "File #{problem_filename} does not match problem pattern"
     end
   end
