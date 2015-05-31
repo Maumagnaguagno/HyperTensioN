b72ced20b499d18a641548728aba89d3f97a78e1
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-05-31 05:55:07 -0300

    Object mining for PDDL
    Extract objects from tasks as they may not appear in the initial or goal
    states.

--- compilers/PDDL_Compiler.rb
+++ compilers/PDDL_Compiler.rb
@@ -52,6 +52,7 @@ module PDDL_Compiler
       objects.push(*pre.drop(1))
       start_str << "    (#{pre.join(' ')})\n"
     }
+    tasks.each {|pred,*terms| objects.push(*terms)}
     goal_str = ''
     goal_pos.each {|pre|
       objects.push(*pre.drop(1))
