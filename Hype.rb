a1caad957007bb3e2a1a220af3aa07ea329b3b26
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-06-04 18:42:43 -0300

    Small optimization for debug ARGV

--- Hype.rb
+++ Hype.rb
@@ -196,7 +196,7 @@ if $0 == __FILE__
         if type and type != 'print'
           if type == 'run' or type == 'debug'
             Hype.compile(domain, problem, 'rb')
-            ARGV.clear << '-d' if type == 'debug'
+            ARGV[0] = '-d' if type == 'debug'
             require "#{Hype::FILEPATH}/#{problem}"
           else Hype.compile(domain, problem, type)
           end
