b2bcc1be299cb7b0b32b5a7e599dcd1a4feb7784
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-07-13 19:14:31 -0300

    Crazy queens
    Lots of speed-ups with the new free_collum predicate, less tests and
    constants.

--- examples/n_queens/N_Queens.rb
+++ examples/n_queens/N_Queens.rb
@@ -4,6 +4,9 @@ module N_Queens
   include Hypertension
   extend self
 
+  CLEAR = ?.
+  QUEEN = ?Q
+
   #-----------------------------------------------
   # Domain
   #-----------------------------------------------
@@ -27,11 +30,12 @@ module N_Queens
       [],
       # Add effects
       [
-        ['board', x, y, 'Q']
+        ['board', x, y, QUEEN]
       ],
       # Del effects
       [
-        ['board', x, y, '.']
+        ['board', x, y, CLEAR],
+        ['free_collumn', x]
       ]
     )
   end
@@ -46,50 +50,54 @@ module N_Queens
       yield []
       return
     end
+    yi = c.pred
     # Free variables
     x = ''
-    y = c.pred.to_s # Closed collumn boost
+    y = yi.to_s # Closed row boost
     # Generate unifications
     generate(
       # Positive preconditions
       [
-        ['board', x, y, '.'],
+        ['free_collumn', x], # Check this first to limit faster the possible values
+        ['board', x, y, CLEAR]
       ],
       # Negative preconditions
       [], x
     ) {
+      # No need to test x == i, free collumn test
+      # No need to test y == j, every piece has their row
       xi = x.to_i
-      yi = y.to_i
-      next if @state['board'].any? {|i,j,p|
-        p == 'Q' and x == i || y == j || (xi - i.to_i).abs == (yi - j.to_i).abs
-      }
+      next if @state['board'].any? {|i,j,p| p == QUEEN and (xi - i.to_i).abs == (yi - j.to_i).abs}
       yield [
         ['put_piece', x, y],
-        ['solve', c.pred]
+        ['solve', yi]
       ]
-    }
-  end
-
-  def solve_board(size)
-    board = []
-    size.times {|i| size.times {|j| board << [i.to_s, j.to_s, '.']}}
-    problem(
-      # Start
-      {'board' => board},
-      # Tasks
-      [['solve', size]]
-    )
-    board = []
-    @state['board'].each {|x,y,p| board[x.to_i + y.to_i * size] = p}
-    index = 0
-    size.times {
-      size.times {
-        print board[index]
-        index += 1
-      }
-      puts
     }
   end
 end
 
-N_Queens.solve_board(ARGV.first ? ARGV.first.to_i : 8)
+begin
+  # NxN board
+  size = ARGV.first ? ARGV.first.to_i : 8
+  board = []
+  size.times {|i| size.times {|j| board << [i.to_s, j.to_s, N_Queens::CLEAR]}}
+  N_Queens.problem(
+    # Start
+    {
+      'board' => board,
+      'free_collumn' => Array.new(size) {|i| [i.to_s]}
+    },
+    # Tasks
+    [
+      ['solve', size]
+    ]
+  )
+  # Draw
+  board = []
+  N_Queens.state['board'].each {|x,y,p| board[x.to_i + y.to_i * size] = p}
+  index = -1
+  size.times {
+    size.times {print board[index += 1]}
+    puts
+  }
+end
