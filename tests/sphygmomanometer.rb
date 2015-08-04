ea3ca3d883d4b25b2a6f6a2330f832b94b8ea6ab
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-08-03 21:46:26 -0300

    Expanded tests

--- tests/sphygmomanometer.rb
+++ tests/sphygmomanometer.rb
@@ -18,7 +18,19 @@ class Sphygmomanometer < Test::Unit::TestCase
     }
   end
 
-  def test_planning
+  #-----------------------------------------------
+  # Planning
+  #-----------------------------------------------
+
+  def test_planning_empty
+    Hypertension.state = original_state = simple_state
+    Hypertension.domain = {}
+    assert_equal([], Hypertension.planning([]))
+    # No state was created
+    assert_same(original_state, Hypertension.state)
+  end
+
+  def test_planning_success
     expected_plan = [
       ['put_piece', '0', '7'],
       ['put_piece', '4', '6'],
@@ -35,14 +47,29 @@ class Sphygmomanometer < Test::Unit::TestCase
     assert_equal({'queen' => expected_plan, 'free_collumn' => []}, N_Queens.state)
   end
 
-  def test_generate
+  def test_planning_failure
+    N_Queens.solve(8, false, false)
+    assert_equal(false, N_Queens.planning([['solve',1]]))
+  end
+
+  def test_planning_exception
     Hypertension.state = simple_state
-    # Pointers
+    Hypertension.domain = {}
+    assert_raise(RuntimeError) {Hypertension.planning([['exception_rise']])}
+  end
+
+  #-----------------------------------------------
+  # Generate
+  #-----------------------------------------------
+
+  def test_generate
+    expected = ['1','2','3'].product(['4','5'], ['c'])
+    Hypertension.state = simple_state
+    # Free variables
     x = ''
     y = ''
     z = ''
     # Generate x y z based on state and preconditions
-    generated = []
     Hypertension.generate(
       [
         ['a', x],
@@ -51,20 +78,21 @@ class Sphygmomanometer < Test::Unit::TestCase
       ],
       [], x, y, z
     ) {
-      # Save a copy of pointer values
-      generated << [x.dup, y.dup, z.dup]
+      assert_equal(expected.shift, [x,y,z])
     }
-    # Compare with expected
-    expected = ['1','2','3'].product(['4','5'], ['c'])
-    assert_equal(expected, generated)
+    assert_equal(true, expected.empty?)
   end
 
-  def test_apply_operator_null
+  #-----------------------------------------------
+  # Apply operator
+  #-----------------------------------------------
+
+  def test_apply_operator_empty
     Hypertension.state = original_state = simple_state
     # Successfully applied
     assert_equal(true, Hypertension.apply_operator([['a','1']],[['a','x']],[],[]))
     # New state was created
-    assert_equal(false, original_state.equal?(Hypertension.state))
+    assert_not_same(original_state, Hypertension.state)
     # Same content
     assert_equal(true, original_state == Hypertension.state)
   end
@@ -74,7 +102,7 @@ class Sphygmomanometer < Test::Unit::TestCase
     # Successfully applied
     assert_equal(true, Hypertension.apply_operator([['a','1']],[['a','x']],[['a','y']],[['a','y']]))
     # New state was created
-    assert_equal(false, original_state.equal?(Hypertension.state))
+    assert_not_same(original_state, Hypertension.state)
     # Delete effects must happen before addition, otherwise the effect nullifies itself
     expected = simple_state
     expected['a'] << ['y']
@@ -83,30 +111,34 @@ class Sphygmomanometer < Test::Unit::TestCase
 
   def test_apply_operator_failure
     Hypertension.state = original_state = simple_state
-    # Successfully applied
+    # Precondition failure
     assert_equal(nil, Hypertension.apply_operator([],[['a','2']],[['a','y']],[]))
     # No state was created
-    assert_equal(true, original_state.equal?(Hypertension.state))
+    assert_same(original_state, Hypertension.state)
   end
+
+  #-----------------------------------------------
+  # Applicable?
+  #-----------------------------------------------
 
   def test_applicable_empty
     Hypertension.state = original_state = simple_state
     assert_equal(true, Hypertension.applicable?([],[]))
     # No state was created
-    assert_equal(true, original_state.equal?(Hypertension.state))
+    assert_same(original_state, Hypertension.state)
   end
 
   def test_applicable_success
     Hypertension.state = original_state = simple_state
     assert_equal(true, Hypertension.applicable?([['a','1']],[['a','x']]))
     # No state was created
-    assert_equal(true, original_state.equal?(Hypertension.state))
+    assert_same(original_state, Hypertension.state)
   end
 
   def test_applicable_failure
     Hypertension.state = original_state = simple_state
     assert_equal(false, Hypertension.applicable?([['a','1']],[['a','2']]))
     # No state was created
-    assert_equal(true, original_state.equal?(Hypertension.state))
+    assert_same(original_state, Hypertension.state)
   end
 end
