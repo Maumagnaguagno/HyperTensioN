3a8ad0469ffb208f8c0319096b8b7b235d2f5d20
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-10-09 06:45:35 -0300

    Loop once over operators
    Single line descriptions also help a lot to keep output clean

--- extensions/Wise.rb
+++ extensions/Wise.rb
@@ -2,52 +2,57 @@ module Wise
   extend self
 
   def apply(operators, methods, predicates, state, tasks, goal_pos, goal_not, debug = true)
-    puts 'Fast'.center(50,'-'), 'Removing unused propositions from initial state' if debug
+    puts 'Wise'.center(50,'-') if debug
     sep = ' '
     state.reject! {|pro|
       unless predicates.include?(pro.first)
-        puts "  (#{pro.join(sep)})" if debug
+        puts "Initial state proposition removed: (#{pro.join(sep)})" if debug
         true
       end
     }
-    puts 'Removing precondition contained effects' if debug
     operators.each {|op|
-      puts "  #{op.first}"
-      op[4].reject! {|eff|
-        if op[2].include?(eff)
-          puts "    (#{eff.join(sep)})" if debug
+      name, parameters, precond_pos, precond_not, effect_add, effect_del = op
+      # Variable prefix
+      parameters.each {|var|
+        unless var.start_with?('?')
+          puts "#{name} parameter #{var} modified to ?#{var}" if debug
+          var.insert(0,'?')
+        end
+      }
+      2.upto(5) {|i|
+        op[i].each {|pro|
+          pro.drop(1).each {|term|
+            if term.start_with?('?') and not parameters.include?(term)
+              puts "#{name} never declared variable #{term} from (#{pro.join(sep)}), adding to parameters" if debug
+              parameters << term
+            elsif parameters.include?("?#{term}")
+              puts "#{name} contains probable variable #{term} from (#{pro.join(sep)}), modified to ?#{term}" if debug
+              term.insert(0,'?')
+            end
+          }
+        }
+      }
+      # Effect contained in precondition
+      effect_add.reject! {|eff|
+        if precond_pos.include?(eff)
+          puts "#{name} effect removed: (#{eff.join(sep)})" if debug
           true
         end
       }
-      op[5].reject! {|eff|
-        if op[3].include?(eff)
-          puts "    (not (#{eff.join(sep)}))" if debug
+      effect_del.reject! {|eff|
+        if precond_not.include?(eff)
+          puts "#{name} effect removed: (not (#{eff.join(sep)}))" if debug
           true
         end
       }
+      # Unknown previous state of predicate
+      if debug
+        (precond_all = precond_pos + precond_not).uniq!
+        side_effects = effect_add - precond_all
+        side_effects.each {|pro| puts "#{name} contains side effect: (#{pro.join(sep)})"} unless side_effects.empty?
+        side_effects = effect_del - precond_all
+        side_effects.each {|pro| puts "#{name} contains side effect: (not (#{pro.join(sep)}))"} unless side_effects.empty?
+      end
     }
-    if debug
-      puts 'Side effects'
-      operators.each {|op|
-        puts "  #{op.first}"
-        (preconditions = op[2] + op[3]).uniq!
-        (objects = preconditions.inject([]) {|s,pro| s.concat(pro.drop(1))}).uniq!
-        side_effects = op[4] - preconditions
-        side_effects.each {|pro| puts "    (#{pro.join(sep)})\n      free objects: #{(pro.drop(1) - objects).join(sep)}"} unless side_effects.empty?
-        side_effects = op[5] - preconditions
-        side_effects.each {|pro| puts "    (not (#{pro.join(sep)}))\n      free objects: #{(pro.drop(1) - objects).join(sep)}"} unless side_effects.empty?
-      }
-      puts 'Variable identified as constant'
-      operators.each {|op|
-        puts "  #{op.first}"
-        op[1].any? {|var| puts "    missing '?' in #{var}" unless var.start_with?('?')}
-        2.upto(5) {|i| op[i].each {|pro| pro.drop(1).each {|term| puts "    missing '?' in #{term} on (#{pro.join(sep)})" if op[1].include?("?#{term}")}}}
-      }
-    end
-    # TODO check if constant term is not a variable with missing prefix
-    # TODO count how many times am operator is used by the required tasks (from none to recursive)
-    # TODO generate duplicates of such operators
-    # TODO cluster sequential operators
-    # TODO clean preconditions based on hierarchy
   end
 end
