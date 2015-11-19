0c21a5dda739d511015743bdb8386c78bbe3334c
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-11-19 05:08:11 -0200

    Briefcase example added
    Briefcase must be expanded into a full domain.

--- examples/logic/Logic.rb
+++ examples/logic/Logic.rb
@@ -15,6 +15,9 @@ module Logic
   end
 end
 
+Logic.state = {:number => [['1'],['2'],['3']]}
+
+# Verbose methods
 def not_zero?(x)
   x.to_i != 0
 end
@@ -27,8 +30,7 @@ def four?(x)
   x.to_i == 4
 end
 
-Logic.state = {:number => [['1'],['2'],['3']]}
-
+# Query forall
 x = ''
 puts "For all numbers x, x != 0: #{Logic.forall?([[:number, x]], [], x) {not_zero?(x)}}"
 x = ''
@@ -38,6 +40,7 @@ puts "For all numbers x, x == 4: #{Logic.forall?([[:number, x]], [], x) {four?(x
 x = ''
 puts "For all numbers x, x is odd or even: #{Logic.forall?([[:number, x]], [], x) {x.to_i.odd? or x.to_i.even?}}"
 
+# Query exists
 x = ''
 puts "There exists a number x, x != 0: #{Logic.exists?([[:number, x]], [], x) {not_zero?(x)}}"
 x = ''
@@ -46,3 +49,54 @@ x = ''
 puts "There exists a number x, x == 4: #{Logic.exists?([[:number, x]], [], x) {four?(x)}}"
 x = ''
 puts "There exists a number x, x is odd and even: #{Logic.exists?([[:number, x]], [], x) {x.to_i.odd? and x.to_i.even?}}"
+
+# Apply forall
+puts 'Move briefcase and all its content, rotten cookie is left behind'
+Logic.state = {
+  :at => [
+    ['red_briefcase', 'a'],
+    ['cookie', 'a'],
+    ['rotten_cookie', 'a'],
+    ['documents', 'a']
+  ],
+  :in => [
+    ['cookie', 'red_briefcase'],
+    ['rotten_cookie', 'thrash'],
+    ['documents', 'red_briefcase']
+  ]
+}
+
+def move_briefcase(briefcase, from, to)
+  if Logic.applicable?(
+    # Positive preconditions
+    [
+      [:at, briefcase, from]
+    ],
+    # Negative preconditions
+    [
+      [:at, briefcase, to]
+    ]
+  )
+    add_effects = [[:at, briefcase, to]]
+    del_effects = [[:at, briefcase, from]]
+    object = ''
+    Logic.forall?(
+      # Positive preconditions
+      [
+        [:at, object, from],
+        [:in, object, briefcase]
+      ],
+      # Negative preconditions
+      [], object
+    ) {
+      obj_dup = object.dup
+      add_effects << [:at, obj_dup, to]
+      del_effects << [:at, obj_dup, from]
+    }
+    Logic.apply(add_effects, del_effects)
+  end
+end
+
+p Logic.state[:at]
+move_briefcase('red_briefcase','a','b')
+p Logic.state[:at]
