04427f77c6b83c4535c5b335784cbd42e2d8d4ab
Mau Magnaguagno <mauricio_c_m@hotmail.com>
2015-12-08 08:18:01 -0200

    Quantifiers can be required

--- examples/experiments/Quantifiers.rb
+++ examples/experiments/Quantifiers.rb
@@ -18,40 +18,41 @@ def exists?(precond_pos, precond_not, *free)
 end
 
 #-----------------------------------------------
-# Examples
+# Main
 #-----------------------------------------------
+if $0 == __FILE__
+  @state = {:number => [['1'],['2'],['3']]}
 
-@state = {:number => [['1'],['2'],['3']]}
+  # Verbose methods
+  def not_zero?(x)
+    x.to_i != 0
+  end
 
-# Verbose methods
-def not_zero?(x)
-  x.to_i != 0
+  def not_one?(x)
+    x.to_i != 1
+  end
+
+  def four?(x)
+    x.to_i == 4
+  end
+
+  # Query forall
+  x = ''
+  puts "For all numbers x, x != 0: #{forall?([[:number, x]], [], x) {not_zero?(x)}}"
+  x = ''
+  puts "For all numbers x, x != 1: #{forall?([[:number, x]], [], x) {not_one?(x)}}"
+  x = ''
+  puts "For all numbers x, x == 4: #{forall?([[:number, x]], [], x) {four?(x)}}"
+  x = ''
+  puts "For all numbers x, x is odd or even: #{forall?([[:number, x]], [], x) {x.to_i.odd? or x.to_i.even?}}"
+
+  # Query exists
+  x = ''
+  puts "There exists a number x, x != 0: #{exists?([[:number, x]], [], x) {not_zero?(x)}}"
+  x = ''
+  puts "There exists a number x, x != 1: #{exists?([[:number, x]], [], x) {not_one?(x)}}"
+  x = ''
+  puts "There exists a number x, x == 4: #{exists?([[:number, x]], [], x) {four?(x)}}"
+  x = ''
+  puts "There exists a number x, x is odd and even: #{exists?([[:number, x]], [], x) {x.to_i.odd? and x.to_i.even?}}"
 end
-
-def not_one?(x)
-  x.to_i != 1
-end
-
-def four?(x)
-  x.to_i == 4
-end
-
-# Query forall
-x = ''
-puts "For all numbers x, x != 0: #{forall?([[:number, x]], [], x) {not_zero?(x)}}"
-x = ''
-puts "For all numbers x, x != 1: #{forall?([[:number, x]], [], x) {not_one?(x)}}"
-x = ''
-puts "For all numbers x, x == 4: #{forall?([[:number, x]], [], x) {four?(x)}}"
-x = ''
-puts "For all numbers x, x is odd or even: #{forall?([[:number, x]], [], x) {x.to_i.odd? or x.to_i.even?}}"
-
-# Query exists
-x = ''
-puts "There exists a number x, x != 0: #{exists?([[:number, x]], [], x) {not_zero?(x)}}"
-x = ''
-puts "There exists a number x, x != 1: #{exists?([[:number, x]], [], x) {not_one?(x)}}"
-x = ''
-puts "There exists a number x, x == 4: #{exists?([[:number, x]], [], x) {four?(x)}}"
-x = ''
-puts "There exists a number x, x is odd and even: #{exists?([[:number, x]], [], x) {x.to_i.odd? and x.to_i.even?}}"
