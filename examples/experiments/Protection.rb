fa739c83b09c179c353c94456500fa547301460f
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-03-17 22:01:58 -0300

    Fix protected? and add example

--- examples/experiments/Protection.rb
+++ examples/experiments/Protection.rb
@@ -21,10 +21,24 @@ def unprotect(protection_pos, protection_not)
 end
 
 def protected?(effect_add, effect_del)
-  effect_add.none? {|pre| @state[:protection_not].include?(pre)} and effect_del.none? {|pre| @state[:protection_pos].include?(pre)}
+  effect_add.any? {|pre| @state[:protection_not].include?(pre)} and effect_del.any? {|pre| @state[:protection_pos].include?(pre)}
 end
 
 def apply_protected_operator(precond_pos, precond_not, effect_add, effect_del)
   # Apply operator unless interfere with protected
   apply_operator(precond_pos, precond_not, effect_add, effect_del) unless protected?(effect_add, effect_del)
+end
+
+#-----------------------------------------------
+# Main
+#-----------------------------------------------
+if $0 == __FILE__
+  @state = {:something => [['a'], ['b']]}
+  setup_protection
+  protect([[:something, 'a']], [[:something, 'c']])
+  p @state
+  p apply_protected_operator([], [], [[:something, 'c']], [[:something, 'a']])
+  p @state
+  p apply_protected_operator([], [], [[:something, 'x']], [[:something, 'b']])
+  p @state
 end
