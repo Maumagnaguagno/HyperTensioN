292c78d7bc9e8672cc8e115aca4cb515a5182056
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-07-13 01:29:08 -0300

    Check if call is present
    Otherwise it is an Array term, useful in certain cases.

--- examples/experiments/Formula.rb
+++ examples/experiments/Formula.rb
@@ -40,7 +40,7 @@ def call(expression)
   if expression.empty?
     send(f, value)
   else
-    expression.each {|i| value = value.send(f, i.is_a?(Array) ? call(i) : i)}
+    expression.each {|i| value = value.send(f, i.is_a?(Array) && i.first == :call ? call(i) : i)}
     value
   end
 end
