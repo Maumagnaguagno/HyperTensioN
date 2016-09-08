70ca57b7f826c45ed144ff998895879e6e92914a
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-09-07 23:21:53 -0300

    Initial state may contain unused predicates

--- compilers/Markdown_Compiler.rb
+++ compilers/Markdown_Compiler.rb
@@ -8,6 +8,9 @@ module Markdown_Compiler
   def compile_domain(domain_name, problem_name, operators, methods, predicates, state, tasks, goal_pos, goal_not)
     output = "# #{domain_name.capitalize}\n## Predicates\n"
     predicates.each {|k,v| output << "- #{k}: #{v ? 'mutable' : 'invariant'}\n"}
+    unused_predicates = {}
+    state.each {|pre| unused_predicates[pre.first] = nil unless predicates.include?(pre.first)}
+    unused_predicates.each_key {|pre| output << "- #{pre}: unused\n"}
     output << "\n## Operators"
     operators.each {|op|
       output << "\n#{op.first.capitalize} | #{op[1].join(' ')}\n--- | ---\n***Preconditions*** | ***Effects***\n"
