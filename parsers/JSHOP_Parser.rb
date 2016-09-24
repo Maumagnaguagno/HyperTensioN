31492bd9b68ad003f98d0c81369bd49d9a61d078
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-09-24 13:58:30 -0300

    Raise if user labels within method are redefined
    User labels are used to provide a better debug, should never conflict.
    Still need to fix inter method label redefinitions.

--- parsers/JSHOP_Parser.rb
+++ parsers/JSHOP_Parser.rb
@@ -54,7 +54,12 @@ module JSHOP_Parser
     met.shift
     until met.empty?
       # Optional label, add index for the unlabeled decompositions
-      method << [met.first.instance_of?(String) ? met.shift : "#{name}_#{method.size - 2}", free_variables = [], pos = [], neg = []]
+      if met.first.instance_of?(String)
+        label = met.shift
+        raise "Method #{name} redefined #{label} decomposition" if method.drop(2).assoc(label)
+      else label = "#{name}_#{method.size - 2}"
+      end
+      method << [label, free_variables = [], pos = [], neg = []]
       # Preconditions
       if (group = met.shift) != NIL
         raise "Error with #{name} preconditions" unless group.instance_of?(Array)
