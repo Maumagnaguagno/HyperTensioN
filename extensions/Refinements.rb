a07cf2ad3a9889c3e92a02fffd79980d37fa2dd7
Mau Magnaguagno <maumagnaguagno@gmail.com>
2016-10-17 10:11:37 -0200

    Debug output and cluster to macro in Refinements

--- extensions/Refinements.rb
+++ extensions/Refinements.rb
@@ -6,41 +6,42 @@ module Refinements
   #-----------------------------------------------
 
   def apply(operators, methods, predicates, state, tasks, goal_pos, goal_not, debug = true)
-    puts 'Refinements'.center(50,'-') if debug
-    # Cluster sequential operators
-    cluster = []
+    puts 'Refinements'.center(50,'-'), 'Macro' if debug
+    # Macro sequential operators
+    macro = []
     methods.each {|met|
       met.drop(2).each {|dec|
         new_subtasks = []
         dec[4].each {|subtask|
+          # Add operators to macro and skip methods
           if op = operators.assoc(subtask.first)
-            cluster << [op, subtask.drop(1)]
+            macro << [op, subtask.drop(1)]
           else
-            add_cluster_to_subtasks(operators, cluster, new_subtasks)
+            add_macro_to_subtasks(operators, macro, new_subtasks, debug)
             new_subtasks << subtask
           end
         }
-        add_cluster_to_subtasks(operators, cluster, new_subtasks)
+        add_macro_to_subtasks(operators, macro, new_subtasks, debug)
         dec[4] = new_subtasks
       }
     }
-    # Clean preconditions based on hierarchy
-    # TODO
+    # TODO pull up preconditions based on hierarchy
+    # TODO compress predicate/variable/method/invisible operator names (changes final state description)
   end
 
   #-----------------------------------------------
-  # Add clusters to subtasks
+  # Add macro to subtasks
   #-----------------------------------------------
 
-  def add_cluster_to_subtasks(operators, cluster, new_subtasks)
-    if cluster.size > 1
+  def add_macro_to_subtasks(operators, macro, new_subtasks, debug)
+    if macro.size > 1
       name = 'invisible_'
       parameters = []
       precond_pos = []
       precond_not = []
       effect_add = []
       effect_del = []
-      cluster.each_with_index {|(op,param),i|
+      macro.each_with_index {|(op,param),i|
         # Header
         name << '_and_' unless i.zero?
         name << op.first.sub(/^invisible_/,'')
@@ -48,10 +49,12 @@ module Refinements
         # Preconditions
         op[2].each {|pre|
           pre = pre.map {|p| p.start_with?('?') ? param[op[1].index(p)] : p}
+          puts "  Precond (not (#{pre.join(' ')})) will never be satisfied" if debug and (precond_not.include?(pre) or effect_del.include?(pre))
           precond_pos << pre unless precond_pos.include?(pre) or effect_add.include?(pre)
         }
         op[3].each {|pre|
           pre = pre.map {|p| p.start_with?('?') ? param[op[1].index(p)] : p}
+          puts "  Precond (#{pre.join(' ')}) will never be satisfied" if debug and (precond_pos.include?(pre) or effect_add.include?(pre))
           precond_not << pre unless precond_not.include?(pre) or effect_del.include?(pre)
         }
         # Effects
@@ -66,16 +69,22 @@ module Refinements
         # Duplicate visible operators without preconditions or effects to keep plan consistent
         unless op.first.start_with?('invisible_')
           new_subtasks << param.unshift(name_dup = "#{op.first}_dup")
-          operators << [name_dup, op[1], [], [], [], []] unless operators.assoc(name_dup)
+          unless operators.assoc(name_dup)
+            operators << [name_dup, op[1], [], [], [], []]
+            puts "  Duplicate operator #{op.first}" if debug
+          end
         end
       }
       parameters.uniq!
-      # TODO Compare more than just name, variable usage may generate incompatible versions
-      operators << [name, parameters, precond_pos, precond_not, effect_add, effect_del] unless operators.assoc(name)
+      # TODO name may not be enough to match, variable usage may require a new macro operator
+      unless operators.assoc(name)
+        operators << [name, parameters, precond_pos, precond_not, effect_add, effect_del]
+        puts "  Macro operator #{name}"
+      end
       new_subtasks.unshift([name, *parameters])
-      cluster.clear
-    elsif cluster.size == 1
-      op, param = cluster.shift
+      macro.clear
+    elsif macro.size == 1
+      op, param = macro.shift
       new_subtasks << param.unshift(op.first)
     end
   end
