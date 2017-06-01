333d68a02df1d9f721c0dacf730348b104eff11d
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-06-01 01:19:39 -0300

    Only add non-empty sections in Markdown_Compiler

--- compilers/Markdown_Compiler.rb
+++ compilers/Markdown_Compiler.rb
@@ -11,31 +11,35 @@ module Markdown_Compiler
     unused_predicates = {}
     state.each {|pre| unused_predicates[pre.first] = nil unless predicates.include?(pre.first)}
     unused_predicates.each_key {|pre| output << "- #{pre}: unused\n"}
-    output << "\n## Operators"
-    operators.each {|name,param,precond_pos,precond_not,effect_add,effect_del|
-      output << "\n#{name.capitalize} | #{param.join(' ')}\n--- | ---\n***Preconditions*** | ***Effects***\n"
-      precond_pos.each {|pre| output << "(#{pre.join(' ')}) |#{" **not** (#{pre.join(' ')})" if effect_del.include?(pre)}\n"}
-      precond_not.each {|pre| output << "**not** (#{pre.join(' ')}) |#{" (#{pre.join(' ')})" if effect_add.include?(pre)}\n"}
-      effect_add.each {|pre| output << "|| (#{pre.join(' ')})\n" unless precond_not.include?(pre)}
-      effect_del.each {|pre| output << "|| **not** (#{pre.join(' ')})\n" unless precond_pos.include?(pre)}
-    }
-    output << "\n## Methods"
-    methods.each_with_index {|(name,param,*decompositions),i|
-      output << "\n#{name.capitalize} | #{param.join(' ')} ||\n--- | --- | ---\n***Label*** | ***Preconditions*** | ***Subtasks***"
-      decompositions.each {|dec|
-        output << "\n#{dec.first} ||"
-        index = 0
-        dec[2].each {|pre|
-          output << "\n|| (#{pre.join(' ')}) | #{dec[4][index].join(' ') if dec[4][index]}"
-          index += 1
-        }
-        dec[3].each {|pre|
-          output << "\n|| **not** (#{pre.join(' ')}) | #{dec[4][index].join(' ') if dec[4][index]}"
-          index += 1
-        }
+    unless operators.empty?
+      output << "\n## Operators"
+      operators.each {|name,param,precond_pos,precond_not,effect_add,effect_del|
+        output << "\n#{name.capitalize} | #{param.join(' ')}\n--- | ---\n***Preconditions*** | ***Effects***\n"
+        precond_pos.each {|pre| output << "(#{pre.join(' ')}) |#{" **not** (#{pre.join(' ')})" if effect_del.include?(pre)}\n"}
+        precond_not.each {|pre| output << "**not** (#{pre.join(' ')}) |#{" (#{pre.join(' ')})" if effect_add.include?(pre)}\n"}
+        effect_add.each {|pre| output << "|| (#{pre.join(' ')})\n" unless precond_not.include?(pre)}
+        effect_del.each {|pre| output << "|| **not** (#{pre.join(' ')})\n" unless precond_pos.include?(pre)}
       }
-      output << "\n" if i != methods.size.pred
-    }
+    end
+    unless methods.empty?
+      output << "\n## Methods"
+      methods.each_with_index {|(name,param,*decompositions),i|
+        output << "\n#{name.capitalize} | #{param.join(' ')} ||\n--- | --- | ---\n***Label*** | ***Preconditions*** | ***Subtasks***"
+        decompositions.each {|dec|
+          output << "\n#{dec.first} ||"
+          index = 0
+          dec[2].each {|pre|
+            output << "\n|| (#{pre.join(' ')}) | #{dec[4][index].join(' ') if dec[4][index]}"
+            index += 1
+          }
+          dec[3].each {|pre|
+            output << "\n|| **not** (#{pre.join(' ')}) | #{dec[4][index].join(' ') if dec[4][index]}"
+            index += 1
+          }
+        }
+        output << "\n" if i != methods.size.pred
+      }
+    end
     output
   end
 
@@ -46,16 +50,17 @@ module Markdown_Compiler
   def compile_problem(domain_name, problem_name, operators, methods, predicates, state, tasks, goal_pos, goal_not, domain_filename)
     output = "# #{problem_name.capitalize} of #{domain_name.capitalize}\n## Initial state"
     state.each {|pre| output << "\n- (#{pre.join(' ')})"}
-    output << "\n\n## Tasks"
     unless tasks.empty?
       ordered = tasks.shift
-      output << (ordered ? "\n**ordered**" : "\n**unordered**")
+      output << "\n\n## Tasks" << (ordered ? "\n**ordered**" : "\n**unordered**")
       tasks.each {|task| output << "\n- (#{task.join(' ')})"}
       tasks.unshift(ordered)
     end
-    output << "\n\n## Goal state"
-    goal_pos.each {|pre| output << "\n- (#{pre.join(' ')})"}
-    goal_not.each {|pre| output << "\n- **not** (#{pre.join(' ')})"}
+    unless goal_pos.empty? and goal_not.empty?
+      output << "\n\n## Goal state"
+      goal_pos.each {|pre| output << "\n- (#{pre.join(' ')})"}
+      goal_not.each {|pre| output << "\n- **not** (#{pre.join(' ')})"}
+    end
     output
   end
 end
