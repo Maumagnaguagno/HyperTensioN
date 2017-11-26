ce9c1f5eb62b9f5b52cd5959108f40d870cef391
Mau Magnaguagno <maumagnaguagno@gmail.com>
2017-11-26 20:51:01 -0200

    Add Travel pb5

---
+++ examples/travel/pb5.rb
@@ -0,0 +1,48 @@
+require_relative 'Travel'
+
+plan = Travel.problem(
+  # Start
+  {
+    'at' => [
+      ['me', 'home'],
+      ['taxi', 'park']
+    ],
+    'cash' => [
+      ['me', '20']
+    ],
+    'owe' => [
+      ['me', '0']
+    ],
+    'distance' => [
+      ['home', 'park', '8'],
+      ['home', 'friend', '10'],
+      ['park', 'home', '8'],
+      ['park', 'friend', '2'],
+      ['friend', 'home', '10'],
+      ['friend', 'park', '2']
+    ],
+    'connected' => [
+      ['home', 'park'],
+      ['park', 'home'],
+      ['home', 'friend'],
+      ['friend', 'home'],
+      ['friend', 'park'],
+      ['park', 'friend']
+    ]
+  },
+  # Tasks
+  [
+    ['travel', 'me', 'friend'],
+    ['travel', 'me', 'park']
+  ],
+  # Debug
+  ARGV.first == 'debug'
+)
+
+# Test
+abort('Test failed') if plan != [
+  ['call_taxi', 'park', 'home'],
+  ['ride_taxi', 'me', 'home', 'friend', '6.5'],
+  ['pay_driver', 'me', '20', '6.5'],
+  ['walk', 'me', 'friend', 'park']
+]
