3ba1617fee67a477542184d3a2839b93165ffa68
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-04-05 19:26:21 -0300

    Protection overwrites Hypertension methods

--- examples/experiments/Protection.rb
+++ examples/experiments/Protection.rb
@@ -1,24 +1,34 @@
-#-----------------------------------------------
-# Protection
-#-----------------------------------------------
+require_relative '../../Hypertension'
 
-def protect(protection_pos, protection_not)
-  @state[:protection_pos].concat(protection_pos)
-  @state[:protection_not].concat(protection_not)
-end
+module Hypertension
 
-def unprotect(protection_pos, protection_not)
-  @state[:protection_pos] -= protection_pos
-  @state[:protection_not] -= protection_not
-end
+  alias original_problem problem
 
-def protected?(effect_add, effect_del)
-  effect_add.any? {|pre| @state[:protection_not].include?(pre)} and effect_del.any? {|pre| @state[:protection_pos].include?(pre)}
-end
+  def problem(start, *args)
+    start[:protection_pos] = []
+    start[:protection_not] = []
+    original_problem(start, *args)
+  end
 
-def apply_protected_operator(precond_pos, precond_not, effect_add, effect_del)
-  # Apply effects if preconditions satisfied and not protected
-  apply(effect_add, effect_del) if applicable?(precond_pos, precond_not) and not protected?(effect_add, effect_del)
+  def protect(protection_pos, protection_not)
+    @state[:protection_pos].concat(protection_pos)
+    @state[:protection_not].concat(protection_not)
+  end
+
+  def unprotect(protection_pos, protection_not)
+    @state[:protection_pos] -= protection_pos
+    @state[:protection_not] -= protection_not
+  end
+
+  def protected?(effect_add, effect_del)
+    effect_add.any? {|pre| @state[:protection_not].include?(pre)} and effect_del.any? {|pre| @state[:protection_pos].include?(pre)}
+  end
+
+  alias original_apply apply
+
+  def apply(effect_add, effect_del)
+    original_apply(effect_add, effect_del) unless protected?(effect_add, effect_del)
+  end
 end
 
 #-----------------------------------------------
@@ -26,7 +36,6 @@ end
 #-----------------------------------------------
 if $0 == __FILE__
   require 'test/unit'
-  require_relative '../../Hypertension'
 
   class Protection < Test::Unit::TestCase
     include Hypertension
@@ -44,9 +53,9 @@ if $0 == __FILE__
       assert_equal([['a'],['b']], @state[:something])
       assert_equal([[:something, 'a']], @state[:protection_pos])
       assert_equal([[:something, 'c']], @state[:protection_not])
-      assert_nil(apply_protected_operator([], [], [[:something, 'c']], [[:something, 'a']]))
+      assert_nil(apply_operator([], [], [[:something, 'c']], [[:something, 'a']]))
       assert_equal([['a'],['b']], @state[:something])
-      assert_equal(true, apply_protected_operator([], [], [[:something, 'x']], [[:something, 'b']]))
+      assert_equal(true, apply_operator([], [], [[:something, 'x']], [[:something, 'b']]))
       assert_equal([['a'],['x']], @state[:something])
     end
   end
