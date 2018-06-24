b970172ae9e65dbf9e62d271d8717ddafc2098db
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-06-24 05:54:45 -0300

    Improve Paisley test_flexible_parameters_in_swap

--- tests/paisley.rb
+++ tests/paisley.rb
@@ -2,90 +2,91 @@ require './tests/hypest'
 
 class Paisley < Test::Unit::TestCase
 
+  def cross_operator(parameters, constraint_terms)
+    [
+      ['cross', ['?agent', '?b', *parameters],
+        # Preconditions
+        [
+          ['agent', '?ag'], ['predicate', '?b'], ['adjacent' , *constraint_terms],
+          ['bridge', '?from', '?b', '?to'], ['at', '?ag', '?from'], ['empty', '?to']
+        ],
+        [],
+        # Effects
+        [['at', '?ag', '?to'], ['empty', '?from']],
+        [['at', '?ag', '?from'], ['empty', '?to']]
+      ]
+    ]
+  end
+
+  def swap_cross_methods(parameters, constraint_terms)
+    [
+      ['swap_at_until_at', ['?ag', '?from'],
+        ['base', [],
+          # Preconditions
+          [['at', '?ag', '?from']],
+          [],
+          []
+        ],
+        ['using_cross', constraint_terms,
+          # Preconditions
+          [['at', '?ag', '?current'], ['adjacent', *constraint_terms]],
+          [['at', '?ag', '?from'], ['visited_at', '?ag', '?intermediate']],
+          # Subtasks
+          [
+            ['cross', '?agent', '?b', *parameters],
+            ['invisible_visit_at', '?ag', '?current'],
+            ['swap_at_until_at', '?ag', '?from'],
+            ['invisible_unvisit_at', '?ag', '?current']
+          ]
+        ]
+      ],
+      ['swap_at_until_empty', ['?ag', '?from'],
+        ['base', [],
+          # Preconditions
+          [['empty', '?ag']],
+          [],
+          # Subtasks
+          []
+        ],
+        ['using_cross', constraint_terms,
+          # Preconditions
+          [['at', '?ag', '?current'], ['adjacent', *constraint_terms]],
+          [['at', '?ag', '?from'], ['visited_at', '?ag', '?intermediate']],
+          # Subtasks
+          [
+            ['cross', '?agent', '?b', *parameters],
+            ['invisible_visit_at', '?ag', '?current'],
+            ['swap_at_until_empty', '?ag', '?from'],
+            ['invisible_unvisit_at', '?ag', '?current']
+          ]
+        ]
+      ]
+    ]
+  end
+
   def test_flexible_parameters_in_swap
     # Based on planks domain
+    predicates = {
+      'agent' => false,
+      'predicate' => false,
+      'adjacent' => false,
+      'at' => true,
+      'bridge' => true,
+      'empty' => true
+    }
     methods = []
-    Patterns.apply(
-      # Operators
-      [
-        ['cross', ['?agent', '?b', '?from', '?to'],
-          # Preconditions
-          [
-            ['agent', '?ag'], ['predicate', '?b'], ['adjacent' , '?from', '?to'],
-            ['bridge', '?from', '?b', '?to'], ['at', '?ag', '?from'], ['empty', '?to']
-          ],
-          [],
-          # Effects
-          [['at', '?ag', '?to'], ['empty', '?from']],
-          [['at', '?ag', '?from'], ['empty', '?to']]
-        ]
-      ],
-      # Methods
-      methods,
-      # Predicates
-      {
-        'agent' => false,
-        'predicate' => false,
-        'adjacent' => false,
-        'at' => true,
-        'bridge' => true,
-        'empty' => true
-      },
-      # State
-      [],
-      # Tasks
-      [],
-      # Goal_pos
-      [],
-      # Goal_not
-      []
-    )
-    assert_equal(
-      [
-        ['swap_at_until_at', ['?ag', '?from'],
-          ['base', [],
-            # Preconditions
-            [['at', '?ag', '?from']],
-            [],
-            []
-          ],
-          ['using_cross', ['?current', '?intermediate'],
-            # Preconditions
-            [['at', '?ag', '?current'], ['adjacent', '?current', '?intermediate']],
-            [['at', '?ag', '?from'], ['visited_at', '?ag', '?intermediate']],
-            # Subtasks
-            [
-              ['cross', '?agent', '?b', '?current', '?intermediate'],
-              ['invisible_visit_at', '?ag', '?current'],
-              ['swap_at_until_at', '?ag', '?from'],
-              ['invisible_unvisit_at', '?ag', '?current']
-            ]
-          ]
-        ],
-        ['swap_at_until_empty', ['?ag', '?from'],
-          ['base', [],
-            # Preconditions
-            [['empty', '?ag']],
-            [],
-            # Subtasks
-            []
-          ],
-          ['using_cross', ['?current', '?intermediate'],
-            # Preconditions
-            [['at', '?ag', '?current'], ['adjacent', '?current', '?intermediate']],
-            [['at', '?ag', '?from'], ['visited_at', '?ag', '?intermediate']],
-            # Subtasks
-            [
-              ['cross', '?agent', '?b', '?current', '?intermediate'],
-              ['invisible_visit_at', '?ag', '?current'],
-              ['swap_at_until_empty', '?ag', '?from'],
-              ['invisible_unvisit_at', '?ag', '?current']
-            ]
-          ]
-        ]
-      ],
-      methods
-    )
+
+    Patterns.apply(cross_operator(['?from', '?to'], ['?from', '?to']), methods, predicates, [], [], [], [])
+    assert_equal(swap_cross_methods(['?current', '?intermediate'], ['?current', '?intermediate']), methods)
+    methods.clear
+    Patterns.apply(cross_operator(['?from', '?to'], ['?to', '?from']), methods, predicates, [], [], [], [])
+    assert_equal(swap_cross_methods(['?current', '?intermediate'], ['?intermediate', '?current']), methods)
+    methods.clear
+    Patterns.apply(cross_operator(['?to', '?from'], ['?from', '?to']), methods, predicates, [], [], [], [])
+    assert_equal(swap_cross_methods(['?intermediate', '?current'], ['?current', '?intermediate']), methods)
+    methods.clear
+    Patterns.apply(cross_operator(['?to', '?from'], ['?to', '?from']), methods, predicates, [], [], [], [])
+    assert_equal(swap_cross_methods(['?intermediate', '?current'], ['?intermediate', '?current']), methods)
   end
 
   def test_variable_introduction_in_dependency
