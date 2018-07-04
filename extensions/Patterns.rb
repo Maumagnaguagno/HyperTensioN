006fc84b31a048b9ea10d3c86f4f16a39a8a6c21
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-07-04 17:43:27 -0300

    Refactor goal method sorting in Patterns find_goal_methods

--- extensions/Patterns.rb
+++ extensions/Patterns.rb
@@ -161,12 +161,11 @@ module Patterns
     }
     goal_methods.each {|(type,goal),v|
       v.uniq!
-      for_goal = "_for_#{goal.first}"
-      until_goal = "_until_#{goal.first}"
+      for_goal = "_#{goal.first}"
       # Give priority based on operator relevance to goal
       v.sort_by! {|mets,pred2|
         # Prefer to match goal
-        val = mets.first.end_with?(for_goal) || mets.first.end_with?(until_goal) ? -1 : 0
+        val = mets.first.end_with?(for_goal) ? -1 : 0
         val - mets.drop(2).count {|dec| !dec[4].empty? and op = operators.assoc(dec[4].last.first) and op[type ? 4 : 5].assoc(goal.first)}
       }
       if debug
