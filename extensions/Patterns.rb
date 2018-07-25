d95ead9d6111828ff4c447f470a9f4b5c36a7fc6
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-07-25 13:14:31 -0300

    Optimize symmetric difference in Patterns match_patterns

--- extensions/Patterns.rb
+++ extensions/Patterns.rb
@@ -44,7 +44,7 @@ module Patterns
       # Swap (+- => -+) or (-+ => +-) or (+? => -+) or (?- => -+)
       (precond_pos & effect_del).each {|pre|
         if pre2 = effect_add.assoc(pre.first)
-          cparam = (pre.drop(1) - pre2.drop(1)) | (pre2.drop(1) - pre.drop(1))
+          cparam = (pre - pre2) | (pre2 - pre)
           # TODO constraint may not exist or exist more than once
           if constraint = constraints.find {|i| (cparam - i).empty?}
             swaps[op] = [pre, constraint]
