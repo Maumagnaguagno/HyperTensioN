b360bb4c4011c810545446d334ce68a5033daa9f
Mau Magnaguagno <maumagnaguagno@gmail.com>
2018-10-05 15:10:14 -0300

    Add module Debug to experiments

---
+++ examples/experiments/Debug.rb
@@ -0,0 +1,18 @@
+module Debug
+
+  def breakpoint
+    STDIN.gets
+    true
+  end
+
+  def print(*argv)
+    puts argv.join(' ')
+    true
+  end
+
+  def print_state
+    puts 'State'.center(20,'-')
+    @state.each {|k,v| v.each {|i| puts "(#{k} #{i.join(' ')})"}}
+    true
+  end
+end
