49ed7d66fae13fe569f4d385220efd4ea7ab5a36
Mau Magnaguagno <maumagnaguagno@gmail.com>
2019-05-04 19:55:27 -0300

    Keep original type in ARGV in Hype main

--- Hype.rb
+++ Hype.rb
@@ -199,7 +199,7 @@ if $0 == __FILE__
       ARGV.each {|e| Hype.extend(e)}
       if not type or type == 'print'
         puts Hype.to_s
-      elsif type == 'run' or type == (ARGV[0] = 'debug')
+      elsif type == 'run' or (ARGV[0] = type) == 'debug'
         Hype.compile(domain, problem, 'rb')
         require File.expand_path(problem)
       elsif type != 'nil'
