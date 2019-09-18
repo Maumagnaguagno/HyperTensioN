3dfcf0ab2d210c48c80bbf01b1a1acc07da8dcf9
Mau Magnaguagno <maumagnaguagno@gmail.com>
2019-09-18 17:15:13 -0300

    Stochastic experiment as a mixin module

--- examples/experiments/Stochastic.rb
+++ examples/experiments/Stochastic.rb
@@ -1,18 +1,15 @@
-module Hypertension
-  extend self
+module Stochastic
 
-  alias deterministic_planning planning
   def planning(tasks, level = 0)
     if not tasks.empty? and (d = @domain[tasks.first.first]).instance_of?(Array)
       d.shuffle!
     end
-    deterministic_planning(tasks, level)
+    super
   end
 
-  alias deterministic_generate generate
   def generate(precond_pos, precond_not, *free)
     unifications = []
-    deterministic_generate(precond_pos, precond_not, *free) {unifications << free.map {|i| i.dup}}
+    super {unifications << free.map {|i| i.dup}}
     unifications.shuffle!.each {|values|
       free.zip(values) {|f,v| f.replace(v)}
       yield
