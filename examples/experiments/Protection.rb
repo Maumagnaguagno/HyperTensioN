81811ad67093cccad55898d4fbeacd9704d118a9
Mau Magnaguagno <maumagnaguagno@gmail.com>
2020-11-06 03:07:43 -0300

    Update Protection to use both state representations
    HyperTensioN U still uses the old state representation

--- examples/experiments/Protection.rb
+++ examples/experiments/Protection.rb
@@ -1,23 +1,34 @@
 module Protection
 
+  if defined?(Hypertension_U)
+    PROTECTION_POS = :protection_pos
+    PROTECTION_NOT = :protection_not
+  else
+    PROTECTION_POS = -2
+    PROTECTION_NOT = -1
+  end
+
   def problem(state, *args)
-    state[:protection_pos] = []
-    state[:protection_not] = []
+    if defined?(Hypertension_U)
+      state[PROTECTION_POS] = []
+      state[PROTECTION_NOT] = []
+    else state.push([], [])
+    end
     super
   end
 
   def protect(protection_pos, protection_not)
-    @state[:protection_pos].concat(protection_pos)
-    @state[:protection_not].concat(protection_not)
+    @state[PROTECTION_POS].concat(protection_pos)
+    @state[PROTECTION_NOT].concat(protection_not)
   end
 
   def unprotect(protection_pos, protection_not)
-    @state[:protection_pos] -= protection_pos
-    @state[:protection_not] -= protection_not
+    @state[PROTECTION_POS] -= protection_pos
+    @state[PROTECTION_NOT] -= protection_not
   end
 
   def protected?(effect_add, effect_del)
-    effect_add.any? {|pre| @state[:protection_not].include?(pre)} or effect_del.any? {|pre| @state[:protection_pos].include?(pre)}
+    effect_add.any? {|pre| @state[PROTECTION_NOT].include?(pre)} or effect_del.any? {|pre| @state[PROTECTION_POS].include?(pre)}
   end
 
   def apply(effect_add, effect_del)
@@ -35,36 +46,46 @@ if $0 == __FILE__
   class Protect < Test::Unit::TestCase
     include Protection, Hypertension
 
+    PRE = 0
+
     def test_protection
-      @state = {
-        :pre => [['a'], ['b']],
-        :protection_pos => [],
-        :protection_not => []
-      }
-      protect([[:pre, 'a']], [[:pre, 'c']])
-      assert_equal([['a'],['b']], @state[:pre])
-      assert_equal([[:pre, 'a']], @state[:protection_pos])
-      assert_equal([[:pre, 'c']], @state[:protection_not])
+      if defined?(Hypertension_U)
+        @state = {
+          :pre => [['a'], ['b']],
+          :protection_pos => [],
+          :protection_not => []
+        }
+      else
+        @state = [
+          [['a'], ['b']],
+          [],
+          []
+        ]
+      end
+      protect([[PRE, 'a']], [[PRE, 'c']])
+      assert_equal([['a'],['b']], @state[PRE])
+      assert_equal([[PRE, 'a']], @state[PROTECTION_POS])
+      assert_equal([[PRE, 'c']], @state[PROTECTION_NOT])
       assert_equal(false, protected?([], []))
-      assert_equal(false, protected?([[:pre, 'a']], []))
-      assert_equal(false, protected?([], [[:pre, 'c']]))
-      assert_equal(true, protected?([], [[:pre, 'a']]))
-      assert_equal(true, protected?([[:pre, 'c']], []))
-      assert_equal(true, protected?([[:pre, 'c']], [[:pre, 'a']]))
+      assert_equal(false, protected?([[PRE, 'a']], []))
+      assert_equal(false, protected?([], [[PRE, 'c']]))
+      assert_equal(true, protected?([], [[PRE, 'a']]))
+      assert_equal(true, protected?([[PRE, 'c']], []))
+      assert_equal(true, protected?([[PRE, 'c']], [[PRE, 'a']]))
       assert_equal(true, apply_operator([], [], [], []))
-      assert_nil(apply_operator([], [], [], [[:pre, 'a']]))
-      assert_nil(apply_operator([], [], [[:pre, 'c']], []))
-      assert_equal([['a'],['b']], @state[:pre])
-      assert_equal(true, apply_operator([], [], [[:pre, 'x']], [[:pre, 'b']]))
-      assert_equal([['a'],['x']], @state[:pre])
-      assert_nil(apply_operator([], [], [], [[:pre, 'a']]))
-      assert_equal([[:pre, 'a']], @state[:protection_pos])
-      assert_equal([[:pre, 'c']], @state[:protection_not])
-      unprotect([[:pre, 'a']], [])
-      assert_equal([], @state[:protection_pos])
-      assert_equal([[:pre, 'c']], @state[:protection_not])
-      assert_equal(true, apply_operator([], [], [], [[:pre, 'a']]))
-      assert_equal([['x']], @state[:pre])
+      assert_nil(apply_operator([], [], [], [[PRE, 'a']]))
+      assert_nil(apply_operator([], [], [[PRE, 'c']], []))
+      assert_equal([['a'],['b']], @state[PRE])
+      assert_equal(true, apply_operator([], [], [[PRE, 'x']], [[PRE, 'b']]))
+      assert_equal([['a'],['x']], @state[PRE])
+      assert_nil(apply_operator([], [], [], [[PRE, 'a']]))
+      assert_equal([[PRE, 'a']], @state[PROTECTION_POS])
+      assert_equal([[PRE, 'c']], @state[PROTECTION_NOT])
+      unprotect([[PRE, 'a']], [])
+      assert_equal([], @state[PROTECTION_POS])
+      assert_equal([[PRE, 'c']], @state[PROTECTION_NOT])
+      assert_equal(true, apply_operator([], [], [], [[PRE, 'a']]))
+      assert_equal([['x']], @state[PRE])
     end
   end
 end
