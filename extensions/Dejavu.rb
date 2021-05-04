a43f946d55f47148c8d5f10372e071bade2b3c21
Mau Magnaguagno <maumagnaguagno@gmail.com>
2021-05-04 17:19:41 -0300

    Move direct recursion from HDDL Parser to Dejavu

--- extensions/Dejavu.rb
+++ extensions/Dejavu.rb
@@ -12,7 +12,8 @@ module Dejavu
     tasks.uniq(&:first).each {|t| visit(t.first, methods, knots)}
     tasks.unshift(ordered)
     knots.uniq!
-    knots.each {|name,decomposition,index|
+    knots.each {|method,decomposition,index|
+      name = method.first
       terms = decomposition.last.first(index + 1).inject([]) {|s,i| s | i.drop(1)}
       if decomposition[1].empty? or decomposition.last[index].size == 1 or decomposition.last[index].drop(1).sort! != terms.sort
         name = "#{name}_#{decomposition.first}_#{index}"
@@ -26,6 +27,10 @@ module Dejavu
             [unvisit, terms, [], [], [], [[visited, *terms]]]
           )
         end
+        method.drop(2).each {|dec|
+          break if dec.equal?(decomposition)
+          dec.last.each {|subtask,*sterms| subtask.sub!('visit','mark') if subtask.start_with?('invisible_visit_','invisible_unvisit_') and sterms == terms}
+        }
       end
     }
   end
@@ -34,7 +39,7 @@ module Dejavu
     if visited.include?(method) then true
     elsif method = methods.assoc(method)
       visited[method.first] = nil
-      method.drop(2).each {|decomposition| decomposition.last.each_with_index {|task,index| knots << [method.first, decomposition, index] if task.first != method.first and visit(task.first, methods, knots, visited.dup)}}
+      method.drop(2).each {|decomposition| decomposition.last.each_with_index {|task,index| knots << [method, decomposition, index] if visit(task.first, methods, knots, visited.dup)}}
       false
     end
   end
