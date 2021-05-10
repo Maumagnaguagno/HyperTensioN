e3342277f702828dc217012b17803e906fe95ad2
Mau Magnaguagno <maumagnaguagno@gmail.com>
2021-05-10 02:14:27 -0300

    Add Dejavu direct recursion test

--- tests/again.rb
+++ tests/again.rb
@@ -3,6 +3,74 @@ require './extensions/Dejavu'
 
 class Again < Test::Unit::TestCase
 
+  def test_dejavu_direct_recursion
+    methods = [
+      ['m1', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          []
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m1', '?a', '?b']
+          ]
+        ]
+      ]
+    ]
+    Dejavu.apply(operators = [], methods, predicates = {}, nil, tasks = [true, ['m1','bob','home']], nil, nil)
+    assert_equal(
+      [
+        ['invisible_visit_m1_recursion_0', ['?a', '?b'],
+          # Preconditions
+          [],
+          [],
+          # Effects
+          [['visited_m1_recursion_0', '?a', '?b']],
+          []
+        ],
+        ['invisible_unvisit_m1_recursion_0', ['?a', '?b'],
+          # Preconditions
+          [],
+          [],
+          # Effects
+          [],
+          [['visited_m1_recursion_0', '?a', '?b']]
+        ]
+      ], operators
+    )
+    assert_equal([
+      ['m1', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          []
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [['visited_m1_recursion_0', '?a', '?b']],
+          # Subtasks
+          [
+            ['invisible_visit_m1_recursion_0', '?a', '?b'],
+            ['m1', '?a', '?b'],
+            ['invisible_unvisit_m1_recursion_0', '?a', '?b']
+          ]
+        ]
+      ]
+    ], methods)
+    assert_equal({'visited_m1_recursion_0' => true}, predicates)
+    assert_equal([true, ['m1','bob','home']], tasks)
+  end
+
   def test_dejavu_indirect_recursion
     methods = [
       ['m1', ['?a', '?b'],
@@ -93,14 +161,12 @@ class Again < Test::Unit::TestCase
         ['recursion', [],
           # Preconditions
           [],
-          [
-            ['visited_m2_recursion_0', '?a', '?b']
-          ],
+          [['visited_m2_recursion_0', '?a', '?b']],
           # Subtasks
           [
             ['invisible_visit_m2_recursion_0', '?a', '?b'],
             ['m1', '?a', '?b'],
-            ['invisible_unvisit_m2_recursion_0', '?a', '?b'],
+            ['invisible_unvisit_m2_recursion_0', '?a', '?b']
           ]
         ]
       ]
