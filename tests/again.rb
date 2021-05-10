bafa9a713b7747cb377e90b998991752b01cf87d
Mau Magnaguagno <maumagnaguagno@gmail.com>
2021-05-10 03:48:13 -0300

    Add Dejavu mark test

--- tests/again.rb
+++ tests/again.rb
@@ -25,26 +25,24 @@ class Again < Test::Unit::TestCase
       ]
     ]
     Dejavu.apply(operators = [], methods, predicates = {}, nil, tasks = [true, ['m1','bob','home']], nil, nil)
-    assert_equal(
-      [
-        ['invisible_visit_m1_recursion_0', ['?a', '?b'],
-          # Preconditions
-          [],
-          [],
-          # Effects
-          [['visited_m1_recursion_0', '?a', '?b']],
-          []
-        ],
-        ['invisible_unvisit_m1_recursion_0', ['?a', '?b'],
-          # Preconditions
-          [],
-          [],
-          # Effects
-          [],
-          [['visited_m1_recursion_0', '?a', '?b']]
-        ]
-      ], operators
-    )
+    assert_equal([
+      ['invisible_visit_m1_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [['visited_m1_recursion_0', '?a', '?b']],
+        []
+      ],
+      ['invisible_unvisit_m1_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [],
+        [['visited_m1_recursion_0', '?a', '?b']]
+      ]
+    ], operators)
     assert_equal([
       ['m1', ['?a', '?b'],
         ['base', [],
@@ -87,7 +85,7 @@ class Again < Test::Unit::TestCase
           [],
           # Subtasks
           [
-            ['m2', '?a', '?b', '?c'],
+            ['m2', '?a', '?b', '?c']
           ]
         ]
       ],
@@ -105,32 +103,30 @@ class Again < Test::Unit::TestCase
           [],
           # Subtasks
           [
-            ['m1', '?a', '?b'],
+            ['m1', '?a', '?b']
           ]
         ]
       ]
     ]
     Dejavu.apply(operators = [], methods, predicates = {}, nil, tasks = [true, ['m1','bob','home']], nil, nil)
-    assert_equal(
-      [
-        ['invisible_visit_m2_recursion_0', ['?a', '?b'],
-          # Preconditions
-          [],
-          [],
-          # Effects
-          [['visited_m2_recursion_0', '?a', '?b']],
-          []
-        ],
-        ['invisible_unvisit_m2_recursion_0', ['?a', '?b'],
-          # Preconditions
-          [],
-          [],
-          # Effects
-          [],
-          [['visited_m2_recursion_0', '?a', '?b']]
-        ]
-      ], operators
-    )
+    assert_equal([
+      ['invisible_visit_m2_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [['visited_m2_recursion_0', '?a', '?b']],
+        []
+      ],
+      ['invisible_unvisit_m2_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [],
+        [['visited_m2_recursion_0', '?a', '?b']]
+      ]
+    ], operators)
     assert_equal([
       ['m1', ['?a', '?b'],
         ['base', [],
@@ -146,7 +142,7 @@ class Again < Test::Unit::TestCase
           [],
           # Subtasks
           [
-            ['m2', '?a', '?b', '?c'],
+            ['m2', '?a', '?b', '?c']
           ]
         ]
       ],
@@ -173,5 +169,187 @@ class Again < Test::Unit::TestCase
     ], methods)
     assert_equal({'visited_m2_recursion_0' => true}, predicates)
     assert_equal([true, ['m1','bob','home']], tasks)
+  end
+
+  def test_dejavu_mark
+    methods = [
+      ['m0', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m0', '?a', '?b']
+          ]
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m0', '?a', '?b']
+          ]
+        ]
+      ],
+      ['m1', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m2', '?a', '?b', '?c']
+          ]
+        ],
+        ['recursion', ['?c'],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m2', '?a', '?b', '?c']
+          ]
+        ]
+      ],
+      ['m2', ['?a', '?b', '?c'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          []
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m1', '?a', '?b']
+          ]
+        ]
+      ]
+    ]
+    Dejavu.apply(operators = [], methods, predicates = {}, nil, tasks = [true, ['m0','bob','home'], ['m1','bob','home']], nil, nil)
+    assert_equal([
+      ['invisible_mark_m0_base_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [['visited_m0_base_0', '?a', '?b']],
+        []
+      ],
+      ['invisible_unmark_m0_base_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [],
+        [['visited_m0_base_0', '?a', '?b']]
+      ],
+      ['invisible_visit_m0_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [['visited_m0_recursion_0', '?a', '?b']],
+        []
+      ],
+      ['invisible_unvisit_m0_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [],
+        [['visited_m0_recursion_0', '?a', '?b']]
+      ],
+      ['invisible_visit_m2_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [['visited_m2_recursion_0', '?a', '?b']],
+        []
+      ],
+      ['invisible_unvisit_m2_recursion_0', ['?a', '?b'],
+        # Preconditions
+        [],
+        [],
+        # Effects
+        [],
+        [['visited_m2_recursion_0', '?a', '?b']]
+      ]
+    ], operators)
+    assert_equal([
+      ['m0', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [['visited_m0_base_0', '?a', '?b']],
+          # Subtasks
+          [
+            ['invisible_mark_m0_base_0', '?a', '?b'],
+            ['m0', '?a', '?b'],
+            ['invisible_unmark_m0_base_0', '?a', '?b']
+          ]
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [['visited_m0_recursion_0', '?a', '?b']],
+          # Subtasks
+          [
+            ['invisible_visit_m0_recursion_0', '?a', '?b'],
+            ['m0', '?a', '?b'],
+            ['invisible_unvisit_m0_recursion_0', '?a', '?b']
+          ]
+        ]
+      ],
+      ['m1', ['?a', '?b'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m2', '?a', '?b', '?c']
+          ]
+        ],
+        ['recursion', ['?c'],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          [
+            ['m2', '?a', '?b', '?c']
+          ]
+        ]
+      ],
+      ['m2', ['?a', '?b', '?c'],
+        ['base', [],
+          # Preconditions
+          [],
+          [],
+          # Subtasks
+          []
+        ],
+        ['recursion', [],
+          # Preconditions
+          [],
+          [['visited_m2_recursion_0', '?a', '?b']],
+          # Subtasks
+          [
+            ['invisible_visit_m2_recursion_0', '?a', '?b'],
+            ['m1', '?a', '?b'],
+            ['invisible_unvisit_m2_recursion_0', '?a', '?b']
+          ]
+        ]
+      ]
+    ], methods)
+    assert_equal({'visited_m0_base_0' => true, 'visited_m0_recursion_0' => true, 'visited_m2_recursion_0' => true}, predicates)
+    assert_equal([true, ['m0','bob','home'], ['m1','bob','home']], tasks)
   end
 end
