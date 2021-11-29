bd306b917690fe07686d31b19aaedff466c1995e
Mau Magnaguagno <maumagnaguagno@gmail.com>
2021-11-29 17:06:35 -0300

    Refactor Hanoi problem generator

--- examples/hanoi/hanoi.rb
+++ examples/hanoi/hanoi.rb
@@ -3,9 +3,9 @@ module Hanoi
 
   def generate(discs)
     problem = "(defproblem pb#{discs - 2} hanoi\n  ; Initial state\n  (\n    ; Towers\n"
-    1.upto(discs.pred) {|d| problem << "    (on disk#{d} disk#{d.succ})\n"}
+    2.upto(discs) {|d| problem << "    (on disk#{d - 1} disk#{d})\n"}
     problem << "    (on disk#{discs}  pegA)\n    (top disk1 pegA)\n    (top  pegB pegB)\n    (top  pegC pegC)\n    ; Decrement\n"
-    1.upto(discs) {|d| problem << "    (dec n#{d} n#{d.pred})\n"}
+    1.upto(discs) {|d| problem << "    (dec n#{d} n#{d - 1})\n"}
     problem << "  )\n  ; Task list\n  (\n    (keep_moving n#{discs} pegA pegC pegB)\n  )\n)"
   end
 end
