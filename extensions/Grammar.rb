780977899548e69b2fc4a878138a8dba2a3c78cb
Mau Magnaguagno <maumagnaguagno@gmail.com>
2022-06-02 14:56:31 -0300

    Prefer map! instead of iterate/insert in Grammar

--- extensions/Grammar.rb
+++ extensions/Grammar.rb
@@ -9,14 +9,12 @@ module Grammar
     puts 'Grammar'.center(50,'-'), 'Production rules'
     methods.each {|met|
       parameters = met[1].join(' ')
-      left_token = "#{met.first}(#{parameters})"
-      production_or = []
-      met.drop(2).each {|dec|
-        production_or << "#{met.first}_#{dec.first}(#{parameters})"
+      production_or = met.drop(2).map! {|dec|
         production_and = dec[4].map {|subtask,*terms| "#{subtask}(#{terms.join(' ')})"}
         puts "#{met.first}_#{dec.first}(#{parameters}) ->\n  #{production_and.empty? ? 'empty' : production_and.join(" &\n  ")}"
+        "#{met.first}_#{dec.first}(#{parameters})"
       }
-      puts "#{left_token} ->\n  #{production_or.empty? ? 'empty' : production_or.join(" |\n  ")}"
+      puts "#{met.first}(#{parameters}) ->\n  #{production_or.empty? ? 'empty' : production_or.join(" |\n  ")}"
     }
   end
 end
