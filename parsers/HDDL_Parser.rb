dfbe065f686401bbe7506bf20e3fba89d4cdda5a
Mau Magnaguagno <maumagnaguagno@gmail.com>
2023-05-30 19:49:09 -0300

    Simplify comment removal in parsers

--- parsers/HDDL_Parser.rb
+++ parsers/HDDL_Parser.rb
@@ -15,7 +15,7 @@ module HDDL_Parser
   #-----------------------------------------------
 
   def scan_tokens(filename)
-    (str = IO.read(filename)).gsub!(/;.*$/,'')
+    (str = IO.read(filename)).gsub!(/;.*/,'')
     str.downcase!
     # return str.to_sexpr # require Ichor
     stack = []
