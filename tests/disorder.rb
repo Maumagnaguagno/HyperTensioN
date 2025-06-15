c5fbb1c46c390688c256e93633089cd4e0201a46
Mau Magnaguagno <maumagnaguagno@gmail.com>
2025-06-15 14:56:19 -0300

    Add solvable ordered test to Disorder

--- tests/disorder.rb
+++ tests/disorder.rb
@@ -4,12 +4,18 @@ class Disorder < Test::Unit::TestCase
   include Hypest
 
   DOMAIN = 'examples/basic/basic.jshop'
-  ORDERED = 'examples/basic/pb2.jshop'
+  ORDERED1 = 'examples/basic/pb1.jshop'
+  ORDERED2 = 'examples/basic/pb2.jshop'
   UNORDERED = 'examples/basic/pb3.jshop'
 
-  def test_ordered_ruby_execution
-    interpreted_execution_tests(DOMAIN, ORDERED, 'Hype.rb', 'Planning failed')
-    interpreted_execution_tests(DOMAIN, ORDERED, '-s Hype.rb -IPC', 'Planning failed')
+  def test_ordered_ruby_solvable_execution
+    interpreted_execution_tests(DOMAIN, ORDERED1, 'Hype.rb', "0: drop(kiwi)\n1: pickup(banjo)\nTotal")
+    interpreted_execution_tests(DOMAIN, ORDERED1, '-s Hype.rb -IPC', "\n==>\n1 drop kiwi\n2 pickup banjo\nroot 0\n0 swap banjo kiwi -> case_1 1 2\n<==\n")
+  end
+
+  def test_ordered_ruby_unsolvable_execution
+    interpreted_execution_tests(DOMAIN, ORDERED2, 'Hype.rb', 'Planning failed')
+    interpreted_execution_tests(DOMAIN, ORDERED2, '-s Hype.rb -IPC', 'Planning failed')
   end
 
   def test_unordered_ruby_execution
